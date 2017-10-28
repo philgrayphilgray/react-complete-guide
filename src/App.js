a0a75a3f1d73e720d237b7c2b8b65eab0373589f
Phillip Gray <phillipgray@Alice-Bucks-MacBook.local>
2017-10-28 01:52:01 -0400

    create Person component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Phil", age: 30 },
+      { name: "Bob", age: 50 },
+      { name: "Cheongah", age: 29 }
+    ]
+  };
+  switchNameHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: 30 },
+        { name: "Boba", age: 530 },
+        { name: "Cheongahz", age: 29 }
+      ]
+    });
+  };
   render() {
     return (
       <div className="App">
         <h1>HI I'm a React App</h1>
+        <button onClick={this.switchNameHandler.bind(this, "Phillip")}>
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Phil!")}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler}
+        />
       </div>
     );
+    // return React.createElement(
+    //   "div",
+    //   null,
+    //   React.createElement("h1", { className: "App" }, "Hi im react")
+    // );
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const person = props => {
+  return (
+    <div>
+      <p onClick={props.click}>
+        I'm a {props.name} and I am {Math.floor(Math.random() * props.age)}{" "}
+        years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
+
+export default person;
