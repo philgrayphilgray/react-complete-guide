73eced8e8fc97bc8d9d0a36170a532d96681d79d
philgrayphilgray <philgrayphilgray@Phillips-iMac.local>
2017-11-19 17:32:35 -0500

    complete 97

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import styles from "./Person.css";
 import withClass from "../../../hoc/withClass";
 import Aux from "../../../hoc/Aux";
@@ -36,5 +37,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default withClass(Person, styles.Person);
