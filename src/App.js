7de77d67761fcb3c820b7f37d742ab01353ba295
philgrayphilgray <philgrayphilgray@Phillips-iMac.local>
2017-11-20 08:24:24 -0500

    start with clean boilerplate project

--- src/App.js
+++ src/App.js
@@ -1,74 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import Person from "./Person/Person";
 
 class App extends Component {
-  state = {
-    persons: [
-      { name: "Phil", age: 30 },
-      { name: "Bob", age: 50 },
-      { name: "Cheongah", age: 29 }
-    ]
-  };
-  switchNameHandler = newName => {
-    this.setState({
-      persons: [
-        { name: newName, age: 30 },
-        { name: "Boba", age: 530 },
-        { name: "Cheongahz", age: 29 }
-      ]
-    });
-  };
-
-  nameChangedHandler = event => {
-    this.setState({
-      persons: [
-        { name: "Phil", age: 30 },
-        { name: event.target.value, age: 50 },
-        { name: "Cheongahz", age: 29 }
-      ]
-    });
-  };
   render() {
-    const style = {
-      backgroundColor: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer"
-    };
     return (
       <div className="App">
         <h1>HI I'm .... </h1>
-        <button
-          style={style}
-          onClick={() => this.switchNameHandler("Phillippp!!!")}
-        >
-          Switch Name
-        </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          changed={this.nameChangedHandler}
-          click={this.switchNameHandler.bind(this, "Phil!")}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          click={this.switchNameHandler}
-        />
       </div>
     );
-    // return React.createElement(
-    //   "div",
-    //   null,
-    //   React.createElement("h1", { className: "App" }, "Hi im react")
-    // );
   }
 }
 
