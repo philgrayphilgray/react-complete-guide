05a5df6ef149d3d7ef0e8b1dc1ef8d4397b84f4e
philgrayphilgray <philgrayphilgray@Phillips-iMac.local>
2017-11-20 08:55:08 -0500

    enable css components, add custom google font, customize root title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
-import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>HI I'm .... </h1>
-      </div>
-    );
+    return <div />;
   }
 }
 
