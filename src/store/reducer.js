e5091cd35122333d2f995b423e6aafb0211cb3a1
philgrayphilgray <you@example.comclear>
2017-12-07 16:24:50 -0500

    complete 256, basic redux setup

---
+++ src/store/reducer.js
@@ -0,0 +1,21 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
+        ...state
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+      return {
+        ...state
+      };
+  }
+  return state;
+};
+
+export default reducer;
