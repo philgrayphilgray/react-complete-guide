6d5dc0cbd617a8925ceb895c34c7c0962a32d87c
philgrayphilgray <you@example.comclear>
2017-12-07 19:51:08 -0500

    complete section, complete 261, adjusting checkout and contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import axios from "../../../axios-orders";
 import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
@@ -108,7 +109,7 @@ class ContactData extends Component {
       ].value;
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price, //calculate on the server in real app
       orderData: formData
     };
@@ -201,4 +202,11 @@ class ContactData extends Component {
     );
   }
 }
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  };
+};
+
+export default connect(mapStateToProps)(ContactData);
