c64c696c07ef442d112ac2c202d226c9a6ec0c51
philgrayphilgray <you@example.comclear>
2017-12-08 19:59:18 -0500

    complete section-16

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -4,6 +4,13 @@ const initialState = {
   results: []
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    result => result.id !== action.resultElId
+  );
+  return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
@@ -11,10 +18,7 @@ const reducer = (state = initialState, action) => {
         results: state.results.concat({ id: new Date(), value: action.result })
       });
     case actionTypes.DELETE_RESULT:
-      const updatedArray = state.results.filter(
-        result => result.id !== action.resultElId
-      );
-      return updateObject(state, { results: updatedArray });
+      return deleteResult(state, action);
   }
   return state;
 };
