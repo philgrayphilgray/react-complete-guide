2fd367bc3fce679e35773f701ed10208cb18509a
philgrayphilgray <you@example.comclear>
2017-12-10 22:33:27 -0500

    293, combining reducers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import "./index.css";
@@ -9,10 +9,17 @@ import App from "./App";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import registerServiceWorker from "./registerServiceWorker";
 
+import orderReducer from "./store/reducers/order";
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer
+});
+
 const store = createStore(
-  burgerBuilderReducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
